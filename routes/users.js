8c87b1a151cce83eecfe11ebd1ff959475661a3e
Maxime Rouiller <maxime.rouiller@microsoft.com>
2020-06-11 12:37:10 -0400

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/string', function(req, res, next) {
+  
+  //todo: implement awesome application
+  
+  res.send('Maxime Rouiller, Professional Racoon Extraordinaire');
+
+});
+router.get('/json', function(req, res, next) {
+  
+  //todo: implement awesome application
+  
+  res.send({firstname: 'Maxime', lastname: 'Rouiller', job: 'Professional Racoon'});
+
+});
+
+module.exports = router;
